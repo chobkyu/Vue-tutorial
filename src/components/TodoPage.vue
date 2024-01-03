36d826d0107f6552a578955d0cf4bacaf7d5e74b
chobkyu <98572357+chobkyu@users.noreply.github.com>
2024-01-03 15:55:34 +0900

    todo list

---
+++ src/components/TodoPage.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import MyCompoenent from './MyComponent.vue'
+import TodoListComponent from './TodoListComponent.vue';
+
+let id = 0;
+
+const newTodo = ref('');
+const todos = ref([
+    { id: id++, text: 'HTML 배우기' },
+    { id: id++, text: 'JavaScript 배우기' },
+    { id: id++, text: 'Vue 배우기' }
+]);
+
+
+</script>
+
+<template>
+    <MyCompoenent>
+        <template #name>조병규</template>
+        <template #intro>의 할일이다 임마</template>
+    </MyCompoenent>
+
+    <!-- <InputComponent>
+    
+    </InputComponent>-->
+
+    <TodoListComponent :todolist="todos"/>
+    
+</template>
